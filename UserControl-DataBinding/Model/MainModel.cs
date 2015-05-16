fc2d0179eae20b309d44cbe5e2621c73789c904b
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-05-16 12:04:43 +0200

    add fields

--- UserControl-DataBinding/Model/MainModel.cs
+++ UserControl-DataBinding/Model/MainModel.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UserControl_DataBinding.Model
 {
-    class MainModel
+    class MainModel : INotifyPropertyChanged
     {
+        string first;
+        string last;
+
+        public string firstname
+        {
+            get
+            {
+                return first;
+            }
+
+            set
+            {
+                if (value != first)
+                {
+                    first = value;
+                    RaisePropertyChanged("firstname");
+                }
+            }
+        }
+
+        public string lastname
+        {
+            get
+            {
+                return last;
+            }
+
+            set
+            {
+                if (value != last)
+                {
+                    last = value;
+                    RaisePropertyChanged("lastname");
+                }
+            }
+        }
+
+        public void RaisePropertyChanged(string prop)
+        {
+            if (this.PropertyChanged != null) { this.PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
