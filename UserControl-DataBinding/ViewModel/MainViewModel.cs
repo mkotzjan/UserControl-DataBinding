af7253f37eae0814dcbf7c1e0ad95e68a3080e8a
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2015-05-17 12:15:41 +0200

    Add field

--- UserControl-DataBinding/ViewModel/MainViewModel.cs
+++ UserControl-DataBinding/ViewModel/MainViewModel.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UserControl_DataBinding.Model;
 
 namespace UserControl_DataBinding.ViewModel
 {
     class MainViewModel
     {
+        private List<MainModel> model;
+
+        public List<MainModel> modelList
+        {
+            get
+            {
+                return model;
+            }
+
+            set
+            {
+                if (value != model)
+                {
+                    model = value;
+                    RaisePropertyChanged("modelList");
+                }
+            }
+        }
         public MainViewModel()
         {
+            List<MainModel> model = new List<MainModel>();
 
         }
     }
